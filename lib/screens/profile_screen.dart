b1fac126da5c529dbd58547425161d6155eb4bc8
vineet22vishwakarma@gmail.com <vineet22vishwakarma@gmail.com>
2024-04-10 01:41:19 +0530

    fix bugs

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -4,8 +4,10 @@ import 'dart:io';
 import 'package:chat_app/controllers/auth_controller.dart';
 import 'package:chat_app/widgets/profile_tile.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -19,6 +21,7 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   late FocusNode focusNode;
   File? _image;
+  PlatformFile? _webImage;
   double _uploadPercentage = 0;
   final AuthController _authController = AuthController();
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
@@ -45,6 +48,100 @@ class _ProfileScreenState extends State<ProfileScreen> {
     focusNode.unfocus();
   }
 
+  Future<void> selectWebImage() async {
+    try {
+      FilePickerResult? result =
+          await FilePicker.platform.pickFiles(type: FileType.image);
+
+      if (result == null) return;
+
+      setState(() {
+        _webImage = result.files.first;
+      });
+
+      if (_webImage != null) {
+        UploadTask uploadTask = _storage
+            .ref()
+            .child('ProfilePics')
+            .child(_authController.getCurrentUser()!.uid)
+            .putData(_webImage!.bytes!);
+        StreamSubscription streamSubscription =
+            uploadTask.snapshotEvents.listen((event) {
+          double percentage = (event.bytesTransferred / event.totalBytes) * 100;
+          setState(() {
+            _uploadPercentage = percentage;
+          });
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            duration: _uploadPercentage == 100
+                ? const Duration(seconds: 1)
+                : const Duration(seconds: 2),
+            backgroundColor: Colors.black,
+            content: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _uploadPercentage == 100
+                    ? Text(
+                        'Uploaded!!',
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.inversePrimary,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      )
+                    : Text(
+                        'Uploading...',
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.inversePrimary,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                const SizedBox(height: 5),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: LinearProgressIndicator(
+                          value: _uploadPercentage,
+                          minHeight: 10,
+                          valueColor: const AlwaysStoppedAnimation(Colors.red),
+                          color: Colors.grey,
+                          backgroundColor: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      _uploadPercentage.toString(),
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.inversePrimary,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+        TaskSnapshot taskSnapshot = await uploadTask;
+        String downloadUrl = await taskSnapshot.ref.getDownloadURL();
+
+        streamSubscription.cancel();
+
+        await _firestore
+            .collection('Users')
+            .doc(_authController.getCurrentUser()!.uid)
+            .update({'profilepic': downloadUrl});
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     TextEditingController usernameController = TextEditingController();
@@ -58,13 +155,15 @@ class _ProfileScreenState extends State<ProfileScreen> {
             await ImagePicker().pickImage(source: ImageSource.gallery);
 
         if (image == null) {
-          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-            backgroundColor: Colors.black,
-            content: Text(
-              'Image Not Selected!!!',
-              style: TextStyle(color: Colors.white),
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              backgroundColor: Colors.black,
+              content: Text(
+                'Image Not Selected!!!',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
-          ));
+          );
           return;
         }
 
@@ -182,7 +281,11 @@ class _ProfileScreenState extends State<ProfileScreen> {
               InkWell(
                 borderRadius: BorderRadius.circular(60),
                 onTap: () {
-                  selectImage();
+                  if (kIsWeb) {
+                    selectWebImage();
+                  } else {
+                    selectImage();
+                  }
                 },
                 child: Stack(
                   alignment: Alignment.bottomRight,
